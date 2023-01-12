e23beefb13babcf6220cc036495483d3598959d2
jthebat <taein007@gmail.com>
2023-01-13 07:48:10 +0900

    Chat App Design

---
+++ src/components/Message.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Message = () => {
+    return (
+        <div className="message owner">
+            <div className="messageInfo">
+                <img 
+                    src="https://w.namu.la/s/d6cbb5161fcf2385efe2be379cf43c56e89ed009097c0436d6f17367cd8b18613299c351d7e2f698c83ef5c42c925959f6ebd65013a29b37ab1f092c147e22d232fca66a706a40bba63d330617567223b679ba1bf11355a45fb2c03cea1bdffa91b2ca8a21b4a33b70aa89f4016783ae" 
+                    alt="" 
+                />
+                <span>just now</span>
+            </div>
+            <div className="messageContent">
+                <p>hello</p>
+                <img 
+                    src="https://w.namu.la/s/d6cbb5161fcf2385efe2be379cf43c56e89ed009097c0436d6f17367cd8b18613299c351d7e2f698c83ef5c42c925959f6ebd65013a29b37ab1f092c147e22d232fca66a706a40bba63d330617567223b679ba1bf11355a45fb2c03cea1bdffa91b2ca8a21b4a33b70aa89f4016783ae" 
+                    alt="" 
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Message
